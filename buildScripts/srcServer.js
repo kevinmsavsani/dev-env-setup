5cbd0431ef156c9a26396141d51613b8978d119e
kevinmsavsani <kevinmsavsani@gmail.com>
2021-05-01 20:12:03 +0530

    automation

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
 var port = 3000;
 var app = express();
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  const __dirname = path.resolve();
+  res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
 app.listen(port, function(err) {

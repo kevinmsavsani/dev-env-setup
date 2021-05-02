3c156a66933d665dc9aaad044d5cb96580a283c9
kevinmsavsani <kevinmsavsani@gmail.com>
2021-05-02 19:11:23 +0530

    Linting ESLint added

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,5 +1,3 @@
 import chalk from 'chalk';
 
-/*eslint-disable no-console */
-
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ from 'numeral';
 
 import './index.css';
 
+/*eslint-disable no-console */
+
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
